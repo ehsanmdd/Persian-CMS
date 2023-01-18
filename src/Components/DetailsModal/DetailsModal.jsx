259982292b2c568c5fa3f335c95373308afbeb3c
Ehsan Motamedi <ehsan.mtd@yahoo.com>
2023-01-18 18:05:11 +0330

    Details Modal

--- src/Components/DetailsModal/DetailsModal.jsx
+++ src/Components/DetailsModal/DetailsModal.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import "./DetailsModal.css"
 
-function DetailsModal() {
+function DetailsModal({ onHide }) {
+
+  useEffect(() => {
+    const checkEvent = (event) => {
+      if (event.keyCode === 27) {
+        onHide()
+      }
+    }
+
+    window.addEventListener("keydown" , checkEvent)
+    return () => window.removeEventListener("keydown" , checkEvent) // Remove EventListener from client cache
+
+  })
+
   return (
-    <div>DetailsModal</div>
+    <div className='modal__parent active'>
+      <div className="detailes__modal ">
+        <table className="details__modal__table">
+          <thead>
+            <tr>
+              <th>اسم</th>
+              <th>قیمت</th>
+              <th>محبوبیت</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>لپتاپ</td>
+              <td>12.000.000</td>
+              <td>91</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
--- src/Components/ProductsTable/ProductsTable.jsx
+++ src/Components/ProductsTable/ProductsTable.jsx
@@ -1,48 +1,64 @@
 import React, { useState } from 'react'
 import DeleteModal from '../DeleteModal/DeleteModal'
 import "./ProductsTable.css"
+import DetailsModal from '../DetailsModal/DetailsModal'
 
 function ProductsTable() {
 
-    const [isShowModal, setIsShowModal] = useState(false)
+    const [isShowDeleteModal, setIsShowDeleteModal] = useState(false)
+    const [isShowDetailsModal, setIsShowDetailsModal] = useState(false)
 
     const modalSubmitAction = () => {
-        setIsShowModal(false)
+        setIsShowDeleteModal(false)
     }
     const modalCancelAction = () => {
-        setIsShowModal(false)
+        setIsShowDeleteModal(false)
+    }
+
+    const closeDtailModal = () => {
+        setIsShowDetailsModal(false)
     }
 
     return (
         <>
-            <div className="tabale__container">
-                <table className="products__table">
-                    <thead>
-                        <tr className="product__table__header">
-                            <th className='product__table__header--title'>عکس</th>
-                            <th className='product__table__header--title'>اسم محصول</th>
-                            <th className='product__table__header--title'>قیمت</th>
-                            <th className='product__table__header--title'>موجودی</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="product__table__body">
-                            <td className="product__table__header--info">
-                                <img src="./images/Product/galaxy-s22-ultra-phantom-black.png" alt="product img" className='product__table--img' />
-                            </td>
-                            <td className="product__table__header--info">Galaxy S22 Ultra Phantom Black</td>
-                            <td className="product__table__header--info">25.000.000 میلیون تومان</td>
-                            <td className="product__table__header--info">80</td>
-                            <td className="product__table__header--info">
-                                <button className='product__table--btnEdit'>جزئیات</button>
-                                <button className='product__table--btnDelete' onClick={() => setIsShowModal(!isShowModal)}>حذف</button>
-                                <button className='product__table--btnInfo'>ویرایش</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            {isShowModal && <DeleteModal submitAction={modalSubmitAction} cancleAction={modalCancelAction} />}
+            <table className="products__table">
+                <thead>
+                    <tr className="product__table__header">
+                        <th className='product__table__header--title'>عکس</th>
+                        <th className='product__table__header--title'>اسم محصول</th>
+                        <th className='product__table__header--title'>قیمت</th>
+                        <th className='product__table__header--title'>موجودی</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="product__table__body">
+                        <td className="product__table__header--info">
+                            <img src="./images/Product/galaxy-s22-ultra-phantom-black.png" alt="product img" className='product__table--img' />
+                        </td>
+                        <td className="product__table__header--info">Galaxy S22 Ultra Phantom Black</td>
+                        <td className="product__table__header--info">25.000.000 میلیون تومان</td>
+                        <td className="product__table__header--info">80</td>
+                        <td className="product__table__header--info">
+                            <button className='product__table--btnInfo' onClick={() => setIsShowDetailsModal(true)}>جزئیات</button>
+                            <button className='product__table--btnDelete' onClick={() => setIsShowDeleteModal(true)}>حذف</button>
+                            <button className='product__table--btnEdit'>ویرایش</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            {
+                isShowDeleteModal &&
+                <DeleteModal
+                    submitAction={modalSubmitAction}
+                    cancleAction={modalCancelAction}
+                />
+            }
+            {
+                isShowDetailsModal &&
+                <DetailsModal
+                    onHide={closeDtailModal}
+                />
+            }
         </>
     )
 }
